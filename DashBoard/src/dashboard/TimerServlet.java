89b35f3c995dd1fa3e0c61cf8da866a6e11717fd
Yannick <Yannick@10.0.8.31>
2012-11-09 00:19:53 +0100

    TimerServlet

---
+++ DashBoard/src/dashboard/TimerServlet.java
@@ -0,0 +1,17 @@
+package dashboard;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TimerServlet extends HttpServlet  {
+
+	private ActivityController AC = new ActivityController();
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+	}
+	}
